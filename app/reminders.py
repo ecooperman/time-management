36b77c29f5213dc18e961155e947e0c25c0bb08d
Evan Cooperman <evanjcooperman@gmail.com>
2026-08-19 10:58:23 -0500

    Update push notification check interval to 15 seconds from 60 seconds just to ensure it fires as close to the time set as humanly possible without polling CONSTANTLY.

--- app/reminders.py
+++ app/reminders.py
@@ -15,7 +15,7 @@ from sqlalchemy.orm import Session
 from . import models, push
 from .database import SessionLocal
 
-CHECK_INTERVAL_SECONDS = 60
+CHECK_INTERVAL_SECONDS = 15
 
 
 def _is_due(task: models.Task, now: datetime) -> bool:
